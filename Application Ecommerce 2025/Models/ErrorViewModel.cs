95d563a57d0a4f89a66971053d1a0b98f07d4db2
Tounsi Hbiba <139052540+HbibaTounsi@users.noreply.github.com>
2026-02-27 00:16:26 +0100

    Delete Application Ecommerce 2025

--- Application Ecommerce 2025/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Application_Ecommerce_2025.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
