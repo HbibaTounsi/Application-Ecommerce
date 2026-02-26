b482c653a8feb0d4a17a33c4126498bd10e5e4ea
Tounsi Hbiba <139052540+HbibaTounsi@users.noreply.github.com>
2026-02-27 00:18:11 +0100

    Delete Application-Ecommerce

--- Application-Ecommerce/Models/Category/EditCatgoryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application_Ecommerce.Models.Category
-{
-    public class EditCatgoryViewModel
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
